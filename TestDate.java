9b90b6516f2c0b4d831135208982c10601702615
Nicholas Sharpe <nicholas.sharpe@keyin.com>
2024-09-16 17:04:25 -0230

    Working on Problem#2 and deleting things being worked on later

--- TestDate.java
+++ TestDate.java
@@ -1,3 +1,12 @@
 public class TestDate {
     
+    public static void main(String[] args) {
+
+        Date d1 = new Date(1, 1, 1900);
+
+        Date d2 = new Date(31, 12, 9999);
+
+        System.out.println(d1);
+        System.out.println(d2);
+    }
 }
